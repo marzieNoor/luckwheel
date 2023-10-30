f03d95af2b1dfdebc23d5272d393e048371c90c2
m.nourmohammadi <marzieh.nourmohammadi@gmail.com>
2023-10-30 16:34:30 +0330

    update files

--- luckWheel/build.gradle.kts
+++ luckWheel/build.gradle.kts
@@ -31,6 +31,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    publishing {
+        multipleVariants {
+            allVariants()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
